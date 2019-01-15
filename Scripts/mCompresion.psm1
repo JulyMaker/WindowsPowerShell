2fe0ec6b96c7f28ce44ba90060ac7d52d2c0425c
Julio Martin Saez <julio.martin.saez@gmail.com>
2019-01-15 17:42:20 +0100

    Descomprime borrando comprimidos

--- Scripts/mCompresion.psm1
+++ Scripts/mCompresion.psm1
@@ -26,6 +26,28 @@ Function comprimir{
    }
 }
 
+Function comprime{
+   ls -name
+   $input = Read-Host "Va a comprimir estos ficheros, esta seguro y/n?"
+   $delete = Read-Host "Deseas borrar los originales y/n?"
+   if($input -Match "y")
+   {
+      $files = ls -name; 
+      foreach ( $file in $files )
+      { 
+         $zipname = $file + ".7z" 
+         if($delete -Match "y")
+         {
+            sz a -sdel $zipname $file
+         }else{
+            sz a $zipname $file
+         }
+             
+      }
+   }
+}
+
+# Descomprime y sin borrar
 Function descomprimir{
    ls -name *.zip
    ls -name *.7z
@@ -42,4 +64,25 @@ Function descomprimir{
      }
      
    }
+}
+
+# Cuidado descomprime y borra los ficheros comprimidos
+Function descomprime{
+   ls -name *.zip
+   ls -name *.7z
+   $input = Read-Host "Va a descomprimir estos ficheros, esta seguro y/n?"
+   $folders = Read-Host "Descomprimir en carpetas y/n?"
+   if($input -Match "y")
+   {
+     if($folders -Match "y"){
+   	   sz x *.zip -o*
+       sz x *.7z -o*
+     }else{
+       sz x *.zip
+       sz x *.7z
+     }
+     
+   }
+   rm *.zip
+   rm *.7z
 }
