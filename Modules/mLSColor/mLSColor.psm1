396de99d8c2dbdae1e8dcb326f4b062900fc03f9
JulioUrjc <julio.martin.saez@gmail.com>
2019-11-20 16:12:22 +0100

    grep color

--- Modules/mLSColor/mLSColor.psm1
+++ Modules/mLSColor/mLSColor.psm1
@@ -4,7 +4,7 @@ Set-Alias lsa lsGetColorAndSize
 Set-Alias lsr lsGetColorAndSizeRecursive
 Set-Alias tam getDirSizeRecursive
 
-function lsGetColorAndSize
+Function lsGetColorAndSize
 {
    <#
     .SYNOPSIS
@@ -24,7 +24,7 @@ function lsGetColorAndSize
     Write-Host
 }
 
-function writeColorLS
+Function writeColorLS
 {
         param ([string]$color = "white", $file)
 
@@ -39,7 +39,7 @@ function writeColorLS
         
 }
 
-function lsColor {
+Function lsColor {
 
     param ($dir)
 
@@ -109,7 +109,7 @@ function lsColor {
 }
 
 
-function getDirSize
+Function getDirSize
 {
     param ($dir)
     $bytes = 0
@@ -146,7 +146,7 @@ function getDirSize
 
 
 
-function lsGetColorAndSizeRecursive
+Function lsGetColorAndSizeRecursive
 {
     <#
     .SYNOPSIS
@@ -166,7 +166,7 @@ function lsGetColorAndSizeRecursive
     Write-Host
 }
 
-function lsColorRecursive {
+Function lsColorRecursive {
 
     param ($dir)
 
@@ -273,7 +273,7 @@ function lsColorRecursive {
 }
 
 
-function getDirSizeRecursive
+Function getDirSizeRecursive
 {
     param ($dir)
     $bytes = 0
@@ -308,7 +308,7 @@ function getDirSizeRecursive
     }
 }
 
-function coloresPosibles{
+Function coloresPosibles{
   <#
     .SYNOPSIS
      Muestra colores posibles con su color
@@ -320,9 +320,19 @@ function coloresPosibles{
       coloresPosibles  
   #> 
   [enum]::GetValues([System.ConsoleColor]) | Foreach-Object {Write-Host $_ -ForegroundColor $_}  
+
+  $colors = [enum]::GetValues([System.ConsoleColor])
+  Foreach ($bgcolor in $colors)
+  {
+    Foreach ($fgcolor in $colors) 
+    { 
+      Write-Host "$fgcolor|"  -ForegroundColor $fgcolor -BackgroundColor $bgcolor -NoNewLine 
+    }
+    Write-Host " on $bgcolor"
+  }
 }
 
-function colores
+Function colores
 {
   <#
     .SYNOPSIS
@@ -342,7 +352,6 @@ function colores
    Write-host "$($_.name.padright(23)) = " -NoNewline
    Write-Host $_.Value -ForegroundColor $_.value
   }
-
 } 
 
 #######################################
@@ -501,10 +510,207 @@ Function Import-ConsoleColor {
     }
 } #Import-ConsoleColor
 
+Function Select-ColorString {
+     <#
+    .SYNOPSIS
+      Find the matches in a given content by the pattern and write the matches in color like grep.
+    .NOTES
+      inspired by: https://ridicurious.com/2018/03/14/highlight-words-in-powershell-console/
+    .EXAMPLE
+      > 'aa bb cc', 'A line' | Select-ColorString a
+      Both line 'aa bb cc' and line 'A line' are displayed as both contain "a" case insensitive.
+    .EXAMPLE
+      > 'aa bb cc', 'A line' | Select-ColorString a -NotMatch
+      Nothing will be displayed as both lines have "a".
+    .EXAMPLE
+      > 'aa bb cc', 'A line' | Select-ColorString a -CaseSensitive
+      Only line 'aa bb cc' is displayed with color on all occurrences of "a" case sensitive.
+    .EXAMPLE
+      > 'aa bb cc', 'A line' | Select-ColorString '(a)|(\sb)' -CaseSensitive -BackgroundColor White
+      Only line 'aa bb cc' is displayed with background color White on all occurrences of regex '(a)|(\sb)' case sensitive.
+    .EXAMPLE
+      > 'aa bb cc', 'A line' | Select-ColorString b -KeepNotMatch
+      Both line 'aa bb cc' and 'A line' are displayed with color on all occurrences of "b" case insensitive,
+    and for lines without the keyword "b", they will be only displayed but without color.
+    .EXAMPLE
+      > Get-Content app.log -Wait -Tail 100 | Select-ColorString "error|warning|critical" -MultiColorsForSimplePattern -KeepNotMatch
+      Search the 3 key words "error", "warning", and "critical" in the last 100 lines of the active file app.log and display the 3 key words in 3 colors.
+      For lines without the keys words, hey will be only displayed but without color.
+    .EXAMPLE
+      > Get-Content "C:\Windows\Logs\DISM\dism.log" -Tail 100 -Wait | Select-ColorString win
+      Find and color the keyword "win" in the last ongoing 100 lines of dism.log.
+    .EXAMPLE
+      > Get-WinEvent -FilterHashtable @{logname='System'; StartTime = (Get-Date).AddDays(-1)} | Select-Object time*,level*,message | Select-ColorString win
+      Find and color the keyword "win" in the System event log from the last 24 hours.
+    #>
 
+    [Cmdletbinding(DefaultParametersetName = 'Match')]
+    param(
+        [Parameter(Position = 0)]
+        [ValidateNotNullOrEmpty()]
+        [String]$Pattern = $(throw "$($MyInvocation.MyCommand.Name) : " `
+                + "Cannot bind null or empty value to the parameter `"Pattern`""),
 
+        [Parameter(
+            ValueFromPipeline = $true,
+            HelpMessage = "String or list of string to be checked against the pattern")]
+        [String[]]$Content,
 
-Export-ModuleMember -function lsGetColorAndSize, lsGetColorAndSizeRecursive, coloresPosibles, getDirSizeRecursive, resetShellColors, initShellColors, colores, Test-ConsoleColor, Export-ConsoleColor, Import-ConsoleColor -Alias lsa,lsr,tam
+        [Parameter()]
+        [ValidateSet(
+            'Black',
+            'DarkBlue',
+            'DarkGreen',
+            'DarkCyan',
+            'DarkRed',
+            'DarkMagenta',
+            'DarkYellow',
+            'Gray',
+            'DarkGray',
+            'Blue',
+            'Green',
+            'Cyan',
+            'Red',
+            'Magenta',
+            'Yellow',
+            'White')]
+        [String]$ForegroundColor = 'Red',
+
+        [Parameter()]
+        [ValidateSet(
+            'Black',
+            'DarkBlue',
+            'DarkGreen',
+            'DarkCyan',
+            'DarkRed',
+            'DarkMagenta',
+            'DarkYellow',
+            'Gray',
+            'DarkGray',
+            'Blue',
+            'Green',
+            'Cyan',
+            'Red',
+            'Magenta',
+            'Yellow',
+            'White')]
+        #[ValidateScript( {
+        #        if ($Host.ui.RawUI.BackgroundColor -eq $_) {
+        #            throw "Current host background color is also set to `"$_`", " `
+        #                + "please choose another color for a better readability"
+        #        }
+        #        else {
+        #            return $true
+        #        }
+        #    })]
+        [String]$BackgroundColor = 'DarkMagenta',
+
+        [Parameter()]
+        [Switch]$CaseSensitive,
+
+        [Parameter(
+            HelpMessage = "Available only if the pattern is simple non-regex string " `
+                + "separated by '|', use this switch with fast CPU.")]
+        [Switch]$MultiColorsForSimplePattern,
+
+        [Parameter(
+            ParameterSetName = 'NotMatch',
+            HelpMessage = "If true, write only not matching lines; " `
+                + "if false, write only matching lines")]
+        [Switch]$NotMatch,
+
+        [Parameter(
+            ParameterSetName = 'Match',
+            HelpMessage = "If true, write all the lines; " `
+                + "if false, write only matching lines")]
+        [Switch]$KeepNotMatch
+    )
+
+    begin {
+        $paramSelectString = @{
+            Pattern       = $Pattern
+            AllMatches    = $true
+            CaseSensitive = $CaseSensitive
+        }
+        $writeNotMatch = $KeepNotMatch -or $NotMatch
+
+        [System.Collections.ArrayList]$colorList =  [System.Enum]::GetValues([System.ConsoleColor])
+        $currentBackgroundColor = $Host.ui.RawUI.BackgroundColor
+        $colorList.Remove($currentBackgroundColor.ToString())
+        $colorList.Remove($ForegroundColor)
+        $colorList.Reverse()
+        $colorCount = $colorList.Count
+
+        if ($MultiColorsForSimplePattern) {
+            $patternToColorMapping = [Ordered]@{}
+            # Available only if the pattern is a simple non-regex string separated by '|', use this with fast CPU.
+            # We dont support regex as -Pattern for this switch as it will need much more CPU.
+            # for example searching "error|warn|crtical" these 3 words in a log file.
+
+            $expectedMatches = $Pattern.split("|")
+            $expectedMatchesCount = $expectedMatches.Count
+            if ($expectedMatchesCount -ge $colorCount) {
+                Write-Host "The switch -MultiColorsForSimplePattern is True, " `
+                    + "but there're more patterns than the available colors number " `
+                    + "which is $colorCount, so rotation color list will be used." `
+                    -ForegroundColor Yellow
+            }
+            0..($expectedMatchesCount -1) | % {
+                $patternToColorMapping.($expectedMatches[$_]) = $colorList[$_ % $colorCount]
+            }
+
+        }
+    }
+
+    process {
+        Foreach ($line in $Content) {
+            $matchList = $line | Select-String @paramSelectString
+
+            if (0 -lt $matchList.Count) {
+                if (-not $NotMatch) {
+                    $index = 0
+                    Foreach ($myMatch in $matchList.Matches) {
+                        $length = $myMatch.Index - $index
+                        Write-Host $line.Substring($index, $length) -NoNewline
+
+                        $expectedBackgroupColor = $BackgroundColor
+                        if ($MultiColorsForSimplePattern) {
+                            $expectedBackgroupColor = $patternToColorMapping[$myMatch.Value]
+                        }
+
+                        $paramWriteHost = @{
+                            Object          = $line.Substring($myMatch.Index, $myMatch.Length)
+                            NoNewline       = $true
+                            ForegroundColor = $ForegroundColor
+                            BackgroundColor = $expectedBackgroupColor
+                        }
+                        Write-Host @paramWriteHost
+
+                        $index = $myMatch.Index + $myMatch.Length
+                    }
+                    Write-Host $line.Substring($index)
+                }
+            }
+            else {
+                if ($writeNotMatch) {
+                    Write-Host "$line"
+                }
+            }
+        }
+    }
+
+    end {
+    }
+}
+
+Function grepc { 
+  PARAM($Pattern, [String]$ForegroundColor = 'Red', [String]$BackgroundColor = 'DarkMagenta', [Switch]$CaseSensitive, [Switch]$NotMatch)
+
+ ls -r -i * | Select-String $Pattern | Select-ColorString $Pattern -ForegroundColor:$ForegroundColor -BackgroundColor:$BackgroundColor -CaseSensitive:$CaseSensitive -NotMatch:$NotMatch
+
+}
+
+Export-ModuleMember -function lsGetColorAndSize, lsGetColorAndSizeRecursive, coloresPosibles, getDirSizeRecursive, resetShellColors, initShellColors, colores, Test-ConsoleColor, Export-ConsoleColor, Import-ConsoleColor, Select-ColorString, grepc -Alias lsa,lsr,tam
 
 #############  COLORS ###########
 # Black        
