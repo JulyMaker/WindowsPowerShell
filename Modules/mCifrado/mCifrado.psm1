92f7fbdd86cdd081692534aef89b5e7ea4fd91db
JulioUrjc <julio.martin.saez@gmail.com>
2020-10-19 10:35:22 +0200

    check directory hahs

--- Modules/mCifrado/mCifrado.psm1
+++ Modules/mCifrado/mCifrado.psm1
@@ -219,4 +219,49 @@ Function compareHash
   }
 }
 
-Export-ModuleMember -function certificados, borrarCertificado, certificado, cifrar, descifrar, comprobarHashMultimedia, generatedMD5, compareHash
+Function checkDirectoryHash
+{
+    <#
+    .SYNOPSIS
+     Compara el hash de los ficheros de un directorio
+    .DESCRIPTION 
+     Compara el hash de los ficheros de un directorio
+    
+    .EXAMPLE 
+      checkDirectoryHash miDir\miFile.md5
+    #> 
+
+  PARAM([System.IO.FileInfo]$pathFile="E:\personal\hashes\17TDT1EU-DA30_0016.kwi.md5")
+
+  $currentPath= $pwd
+  cd $pathFile.DirectoryName
+  
+  $files = ls $pathFile.DirectoryName -exclude *.md5
+  $listado = get-content $pathFile.Name
+  $hashTable = @{}
+  ForEach ($line in $listado) 
+  {
+      $spliteado = -split $line
+      $hashTable.add($spliteado[1].Remove(0,1), $spliteado[0])
+  }
+  ForEach ($file in $files) 
+  {
+      $hash= (Get-FileHash $file -Algorithm MD5).hash #| Select-Object Hash
+      $pHash = $hashTable[$file.Name]
+      if($pHash -eq $null)
+      {
+        write-host -BackgroundColor Yellow -ForegroundColor Black ('{0} - RENOMBRADO' -f $file.Name)
+        continue
+      } 
+      
+      if($hash -eq $pHash)
+      {
+        write-host -BackgroundColor DarkGreen -ForegroundColor Black ('{0} - CORRECTO' -f $file.Name)
+      }else{
+        Write-Host -BackgroundColor DarkRed -ForegroundColor White ('{0} - INCORRECTO' -f $file.Name)
+      }
+  }
+  cd $currentPath
+}
+
+Export-ModuleMember -function certificados, borrarCertificado, certificado, cifrar, descifrar, comprobarHashMultimedia, generatedMD5, compareHash, checkDirectoryHash
