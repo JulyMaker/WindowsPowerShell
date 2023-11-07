7c7269deb656b68bb8930982fb036b1518a8b3e7
Julio Martín <julio.martin@3ds.com>
2023-11-07 14:03:50 +0100

    Create folder in screenshot script

--- Scripts/Captura.ps1
+++ Scripts/Captura.ps1
@@ -6,6 +6,7 @@ function screenshot([Drawing.Rectangle]$bounds, $path)
 	$bmp = New-Object Drawing.Bitmap $bounds.width, $bounds.height
 	$graphics = [Drawing.Graphics]::FromImage($bmp)
 	$graphics.CopyFromScreen($bounds.Location, [Drawing.Point]::Empty, $bounds.size)
+	$path
 	$bmp.Save($path)
 	$graphics.Dispose()
 	$bmp.Dispose()
@@ -15,6 +16,7 @@ for($i=0; $i -le 1720; $i+=200)
 {
 		$i
 		$bounds = [Drawing.Rectangle]::FromLTRB(0, $i, 980, $i+200)
+		# mkdir "C:\Users\jmn6\Desktop\capturas"
 		$fichero='C:\Users\jmn6\Desktop\capturas\captura'+$i+'.png'
 		#Realizar captura
 		screenshot $bounds $fichero
